d69b72c23345a9be0eac2e0ba76c8684fc8fff82
AlexGerwer <asg_akn@yahoo.com>
2019-11-13 13:42:48 -0800

    adding Work from Day 2 on Twitoff

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request#
+from .models import DB, User
 
 # Create an app factory
 
@@ -7,12 +8,28 @@ def create_app():
     app = Flask(__name__)
 
     # Add configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # Change so that changes can be made in the .env file
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    # Eliminate warning message on running the file
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Link the app and the database
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        # return 'Welcome to Twitoff'
+        # return render_template('base.html')
+        # return render_template('base.html', title='Home')
+        return render_template('base.html', title='Home', users=users)
+
+    # Add reset route for convenience
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset', users=[])
+
     return app
